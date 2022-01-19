6caf80b4a9790d4f466659287f31cf12f8031703
James Adams <adamsjlp@gmail.com>
2022-01-19 15:52:52 -0500

    create script to build search index

---
+++ scripts/R/build_index.R
@@ -0,0 +1,57 @@
+library(tidyverse)
+library(rvest)
+library(jsonlite)
+
+ids <- list.files("site", pattern = "\\.html$", full.names = TRUE, recursive = TRUE)
+
+titles <- lapply(1:length(ids), function(i) {
+  cat(paste0("Building index...", round(i / length(ids) * 100, 2), "%\r"))
+  
+  title <- read_html(ids[i]) %>%
+    html_node("title") %>%
+    html_text()
+  
+  title_ascii <- stringi::stri_trans_general(title, "Latin-ASCII")
+  
+  type <- str_extract(ids[i], "/[a-z]*/") %>%
+    str_remove_all("[^a-z]")
+  
+  type <- ifelse(is.na(type), "page", type)
+  
+  text <- read_html(ids[i]) %>%
+    html_nodes("p") %>%
+    html_text() %>%
+    paste(collapse = "; ")
+  
+  tables <- read_html(ids[i]) %>%
+    html_nodes("td") %>%
+    html_text() %>%
+    paste(collapse = "; ")
+  
+  lists <- read_html(ids[i]) %>%
+    html_nodes("li") %>%
+    html_text() %>%
+    paste(collapse = "; ")
+  
+  all_text <- paste(text, tables, lists, sep = "; ") %>%
+    stringi::stri_trans_general("Latin-ASCII") %>%
+    str_remove_all("\\n") %>%
+    str_replace_all("\\s{2,}", replacement = " ")
+  
+  
+  tmp <- tibble(
+    id = paste0("/", ids[i]),
+    type = type,
+    title = title,
+    title_ascii = title_ascii,
+    text = all_text
+  )
+  
+  return(tmp)
+}) %>%
+  bind_rows()
+
+index <- paste0("var data = ", as.character(toJSON(titles, pretty = T)))
+
+write_file(index, "site/index.js")
+
