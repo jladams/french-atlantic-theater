e390c32859cdf0518c8ede9fbd26766c8b4ee317
James Adams <adamsjlp@gmail.com>
2022-02-10 11:16:19 -0500

    Add build_index to runall script

--- scripts/site_from_NER.R
+++ scripts/site_from_NER.R
@@ -26,6 +26,10 @@ remote_db <- FALSE
 source("scripts/R/02_create_db.R")
 rm(list = ls())
 
-# Create Website (this one takes a while)
+# Create Website (this one takes a while, maybe 5 hours)
 source("scripts/R/03_create_site.R")
+rm(list = ls())
+
+# Create search index so that the search boxes on each page work
+source("scripts/R/04_build_index.R")
 rm(list = ls())
